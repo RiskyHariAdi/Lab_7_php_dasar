1084b6704e4fb506fb9e4915df5ee26111359b47
RiskyHariAdi <ikkyhari@gmail.com>
2022-05-18 18:53:24 +0700

    first commit

---
+++ dasar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP Dasar</title>
+</head>
+<body>
+<h1>Belajar PHP Dasar</h1>
+<?php
+echo "Hello World";
+</body>
+</html>
+<?php
+$nim = "312010076";
+$nama = 'Shobahus Solichin';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama";
+?>
---
+++ from_input.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Form Input</title>
+        <meta charset="utf-8">
+
+        <!-- CSS -->
+        <style>
+        body {
+            width: 100%;
+            height: 100vh;
+            margin: 0;
+            font-family: tahoma;
+            font-size: 16px;
+        }
+        h1, p {
+            margin: 1em auto;
+            text-align: center;
+        }
+        form {
+            width: 60vw;
+            max-width: 500px;
+            min-width: 300px;
+            margin: 0 auto;
+            padding-bottom: 2em;
+            }
+        label {
+            display: block;
+            margin: 0.5rem 0;
+        }
+        button[type="submit"] {
+            display: block;
+            width: 60%;
+            margin: 1em auto;
+            height: 2em;
+            font-size: 1.1rem;
+            background-color: #00FF00;
+            border-color: white;
+            min-width: 300px;
+        }
+        </style>
+    </head>
+
+    <body>
+        <h1 id="title">Survey Formulir Pekerjaan dan Gaji</h1>
+        <p id="description"><i>Melansir dari situs <a href="https://www.jobstreet.co.id/id/job-search/it-jobs/"> jobstreet.co.id</a></i></p>
+
+        <form id="survey-form" action="" method="POST">
+            <fieldset>
+                <label>Nama: 
+                    <input type="text" name="nama" required/>
+                </label>
+                <label>Tanggal lahir: 
+                    <input type="date" name="date">
+                </label>
+                <label>Pekerjaan: 
+                    <label>
+                    <input type="radio" name="job" value="0"/>System Analyst
+                    </label>
+                    <label>
+                    <input type="radio" name="job" value="1"/>IT Support
+                    </label>
+                    <label>
+                    <input type="radio" name="job" value="2"/>System Administrator
+                    </label>
+                </label>
+                <button type="submit" name="submit">Submit</button>
+            </fieldset>
+            <fieldset>
+                <?php
+                if( isset($_POST['submit']))
+                {
+                    $nama = $_POST['nama'];
+                    $date = $_POST['date'];
+                    $job = $_POST['job'];
+
+
+                    $date_user = new DateTime($date);
+                    $today =  new DateTime('today');
+                    $usia = $today->diff($date_user)->y;
+
+                    $job_array = ["System Analyst","IT Support","System Administrator"];
+                    $salary_array = ["Rp. 8.000.000","Rp. 6.400.000","Rp. 4.200.000"];
+
+
+                    echo "Halo, ".$nama."<br>Kamu lahir pada tanggal ".$date.", Usia mu ".$usia." tahun";
+                    echo "<br>Pekerjaan yang anda pilih adalah ".$job_array[(int)$job].", dengan penghasilan kurang lebih ".$salary_array[(int)$job];
+                }
+                ?>
+            </fieldset>
+        </form>
+    </body>
+</html>
